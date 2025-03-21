c7d0c3461bba3b9b055dedbd542d8f8ebb5f8b6f
30lima <pedrolimaa354@gmail.com>
2025-03-20 22:44:15 -0300

    feat: adding third exercise

--- script.sql
+++ script.sql
@@ -38,4 +38,41 @@ VALUES (
     20 
 );
 
+--Terceiro exercício
+DECLARE
+    v_employee_id EMPLOYEES.EMPLOYEE_ID%TYPE;
+    v_first_name EMPLOYEES.FIRST_NAME%TYPE;
+    v_last_name EMPLOYEES.LAST_NAME%TYPE;
+    v_email EMPLOYEES.EMAIL%TYPE;
+    v_phone_number EMPLOYEES.PHONE_NUMBER%TYPE;
+    v_hire_date EMPLOYEES.HIRE_DATE%TYPE;
+    v_job_id EMPLOYEES.JOB_ID%TYPE;
+    v_salary EMPLOYEES.SALARY%TYPE;
+    v_commission_pct EMPLOYEES.COMMISSION_PCT%TYPE;
+    v_manager_id EMPLOYEES.MANAGER_ID%TYPE;
+    v_department_id EMPLOYEES.DEPARTMENT_ID%TYPE;
+BEGIN
+    SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, 
+           HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID
+    INTO v_employee_id, v_first_name, v_last_name, v_email, v_phone_number,
+         v_hire_date, v_job_id, v_salary, v_commission_pct, v_manager_id, v_department_id
+    FROM EMPLOYEES
+    WHERE EMPLOYEE_ID = (SELECT MAX(EMPLOYEE_ID) FROM EMPLOYEES);
+
+
+    INSERT INTO AUDIT_EMPLOYEES (
+        EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, 
+        HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID, 
+        AUDIT_USER, AUDIT_ACTION
+    ) 
+    VALUES (
+        v_employee_id, v_first_name, v_last_name, v_email, v_phone_number,
+        v_hire_date, v_job_id, v_salary, v_commission_pct, v_manager_id, v_department_id,
+        'rm558243', 
+        'I'        
+    );
+
+    COMMIT;
+END;
+
 
